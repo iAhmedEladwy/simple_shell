6c4299a757cf2c3f38218440f8b10c7eb450a73e
NouraElRayan5 <nourakhaled385@gmail.com>
2023-11-15 20:59:16 +0200

    done

--- fork.c
+++ fork.c
@@ -17,12 +17,12 @@ return (1);
 }
 my_pid = getpid();
 printf("My pid is %u\n", my_pid);
-				    if (child_pid == 0)
-					        {
-							        printf("(%u) Nooooooooo!\n", my_pid);
-								    }
-				        else
-						    {
+if (child_pid == 0)
+{
+printf("(%u) Nooooooooo!\n", my_pid);
+}
+else
+{
 							            printf("(%u) %u, I am your father\n", my_pid, child_pid);
 								        }
 					    return (0);
