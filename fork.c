95f230d32f77806f9a39f7f60c79dc782361ae85
NouraElRayan5 <nourakhaled385@gmail.com>
2023-11-15 20:47:14 +0200

    done

--- fork.c
+++ fork.c
@@ -12,8 +12,7 @@ pid_t my_pid;
 pid_t child_pid;
 child_pid = fork();
 if (child_pid == -1)
-{
-					            perror("Error:");
+perror("Error:");
 						            return (1);
 							        }
 			    my_pid = getpid();
