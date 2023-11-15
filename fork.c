48f63ded8096c1edbde74646aead9c4e7b378669
NouraElRayan5 <nourakhaled385@gmail.com>
2023-11-15 20:53:09 +0200

    done

--- fork.c
+++ fork.c
@@ -14,9 +14,9 @@ child_pid = fork();
 if (child_pid == -1)
 perror("Error:");
 return (1);
- }
-			    my_pid = getpid();
-			        printf("My pid is %u\n", my_pid);
+}
+my_pid = getpid();
+printf("My pid is %u\n", my_pid);
 				    if (child_pid == 0)
 					        {
 							        printf("(%u) Nooooooooo!\n", my_pid);
