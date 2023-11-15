817cb24db08f3a816d61d62c5a56a6e246d0772a
NouraElRayan5 <nourakhaled385@gmail.com>
2023-11-15 20:44:31 +0200

    done

--- fork.c
+++ fork.c
@@ -8,11 +8,10 @@
  *     */
 int main(void)
 {
-	    pid_t my_pid;
-	        pid_t child_pid;
-
-		    child_pid = fork();
-		        if (child_pid == -1)
+pid_t my_pid;
+pid_t child_pid;
+child_pid = fork();
+if (child_pid == -1)
 				    {
 					            perror("Error:");
 						            return (1);
